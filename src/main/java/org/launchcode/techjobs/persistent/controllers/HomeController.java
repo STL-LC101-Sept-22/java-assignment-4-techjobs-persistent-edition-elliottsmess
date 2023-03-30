f32afff6709ad86cccaece25d37750731eea7c7b
elliottsmess <elliottapearson@gmail.com>
2023-03-30 00:08:04 -0500

    Parts 1, 2 & 3.Done.(i think)

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,12 +14,19 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
 public class HomeController {
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private EmployerRepository employerRepository;
+    @Autowired
+    private SkillRepository skillRepository;
 
     @RequestMapping("")
     public String index(Model model) {
@@ -27,18 +40,34 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers",employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
+                                    Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
 
-        if (errors.hasErrors()) {
+        if (errors.hasErrors ()) {
             model.addAttribute("title", "Add Job");
+            model.addAttribute ("employers", employerRepository.findAll());
+            model.addAttribute ("skills", skillRepository.findAll());
             return "add";
         }
 
+        List<Skill> skillobjs = (List<Skill>) skillRepository.findAllById(skills) ;
+        newJob.setSkills(skillobjs);
+        Optional<Employer> employerobj = employerRepository.findById(employerId) ;
+
+        if (!employerobj.isPresent() ) {
+            model. addAttribute ("title", "Add Job");
+            model. addAttribute("employers", employerRepository.findAll());
+            model.addAttribute("skills", skillRepository.findAll());
+            return "add";
+        }
+        newJob.setEmployer(employerobj.get()) ;
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
